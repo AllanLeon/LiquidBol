b16a4d5fae261866ecd6d0af2bda93e2a42ff191
Allan Leon <allan.leon@outlook.com>
2015-01-12 00:01:18 -0400

    Created Employee class

---
+++ src/com/liquidbol/model/commons/Employee.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.liquidbol.model.commons;
+
+/**
+ * Class that represents a employee.
+ * @author Allan Leon
+ */
+public class Employee extends Person {
+
+    /**
+     * Constructor method.
+     * @param id
+     * @param name
+     * @param lastname
+     * @param address
+     * @param phone
+     * @param phone2
+     * @param email 
+     */
+    public Employee(int id, String name, String lastname, String address, int phone, int phone2, String email) {
+        super(id, name, lastname, address, phone, phone2, email);
+    }
+}
