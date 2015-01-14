dacc22dd7942a5459b8953e7bc8e4d67c4ca2181
Allan Leon <allan.leon@outlook.com>
2015-01-13 20:41:13 -0400

    Created ItemOffer crud

---
+++ src/com/liquidbol/db/persistence/ItemOfferCrud.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.liquidbol.db.persistence;
+
+import com.liquidbol.model.commons.Offer;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Class responsible of all persistence operations related to item offer.
+ * @author Allan Leon
+ */
+public class ItemOfferCrud implements DBCrud<Offer> {
+    
+    private static final Logger LOG = Logger.getLogger(ItemOfferCrud.class.getName());
+
+    private Connection connection;
+
+    @Override
+    public Offer save(Offer element) throws PersistenceException, ClassNotFoundException {
+        try {
+            connection = ConnectionManager.getInstance().getConnection();
+            String insert = "INSERT INTO item_offers(item_type, percentage, "
+                    + "start_date, end_date) VALUES(?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareCall(insert);
+            statement.setString(1, element.getType());
+            statement.setDouble(2, element.getPercentage());
+            statement.setDate(3, element.getStartDate());
+            statement.setDate(4, element.getEndDate());
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new PersistenceException("item offer was not saved");
+            }
+            LOG.info(String.format("item offer: %d successfuly saved", element.getId()));
+            return element;
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new PersistenceException(String.format("Failed to save item offer: %d", element.getId()), ex);
+        } finally {
+            try {
+                ConnectionManager.getInstance().releaseConnection();
+            } catch (SQLException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public Offer find(String id) throws PersistenceException, ClassNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Offer find(int id) throws PersistenceException, ClassNotFoundException {
+        try {
+            String query = "SELECT * FROM item_offers WHERE offer_id = ?";
+            connection = ConnectionManager.getInstance().getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return createElementFromResultSet(resultSet);
+            } else {
+                throw new PersistenceException(String.format("Couldn't find item offer with code %d", id));
+            }
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Failed to read item offer", ex);
+        } finally {
+            try {
+                ConnectionManager.getInstance().releaseConnection();
+            } catch (SQLException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public Offer merge(Offer element) throws PersistenceException, ClassNotFoundException {
+        try {
+            String query = "UPDATE item_offers SET percentage=?, start_date=?, "
+                    + "end_date=? WHERE offer_id=?";
+            PreparedStatement statement = 
+                ConnectionManager.getInstance().getConnection().prepareStatement(query);
+            statement.setDouble(1, element.getPercentage());
+            statement.setDate(2, element.getStartDate());
+            statement.setDate(3, element.getEndDate());
+            statement.setInt(4, element.getId());
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new PersistenceException("item offer was not updated");
+            }
+            return element;
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new PersistenceException(String.format("Failed to update item offer: %d", element.getId()), ex);
+        } finally {
+            try {
+                ConnectionManager.getInstance().releaseConnection();
+            } catch (SQLException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public Collection<Offer> getAll() throws PersistenceException, ClassNotFoundException {
+        try {
+            String query = "SELECT * FROM item_offers";
+            connection = ConnectionManager.getInstance().getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            Collection<Offer> result = new HashSet<>();
+            while (resultSet.next()) {
+                Offer element = createElementFromResultSet(resultSet);
+                result.add(element);
+            }
+            return result;
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Failed to read the item offers", ex);
+        } finally {
+            try {
+                ConnectionManager.getInstance().releaseConnection();
+            } catch (SQLException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public Offer refresh(Offer offer) throws PersistenceException, ClassNotFoundException {
+        return find(offer.getId());
+    }
+
+    @Override
+    public Offer createElementFromResultSet(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt(1);
+        String type = resultSet.getString(2);
+        Double percentage = resultSet.getDouble(3);
+        Date startDate = resultSet.getDate(4);
+        Date endDate = resultSet.getDate(5);
+        LOG.log(Level.FINE, "Creating item offer %d", id);
+        Offer result = new Offer(id, type, percentage, startDate, endDate);
+        return result;
+    }
+    
+}
