af6584f410e405f4f0f83831c2a3aba94e8e9325
Allan Leon <allan.leon@outlook.com>
2015-01-17 18:05:04 -0400

    Created BillPayment class

---
+++ src/com/liquidbol/model/commons/BillPayment.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.liquidbol.model.commons;
+
+import java.sql.Date;
+
+/**
+ * Class that represents a bill payment.
+ * @author Allan Leon
+ */
+public class BillPayment {
+    
+    private int id;
+    private Employee employee;
+    private Date payDate;
+    private Double ammountPaid;
+    private String obs;
+
+    /**
+     * Constructor method.
+     * @param id
+     * @param employee
+     * @param payDate
+     * @param ammountPaid
+     * @param obs 
+     */
+    public BillPayment(int id, Employee employee, Date payDate, Double ammountPaid, String obs) {
+        this.id = id;
+        this.employee = employee;
+        this.payDate = payDate;
+        this.ammountPaid = ammountPaid;
+        this.obs = obs;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the employee
+     */
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    /**
+     * @return the payDate
+     */
+    public Date getPayDate() {
+        return payDate;
+    }
+
+    /**
+     * @return the ammountPaid
+     */
+    public Double getAmmountPaid() {
+        return ammountPaid;
+    }
+
+    /**
+     * @return the obs
+     */
+    public String getObs() {
+        return obs;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @param employee the employee to set
+     */
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    /**
+     * @param payDate the payDate to set
+     */
+    public void setPayDate(Date payDate) {
+        this.payDate = payDate;
+    }
+
+    /**
+     * @param ammountPaid the ammountPaid to set
+     */
+    public void setAmmountPaid(Double ammountPaid) {
+        this.ammountPaid = ammountPaid;
+    }
+
+    /**
+     * @param obs the obs to set
+     */
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BillPayment other = (BillPayment) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+}
