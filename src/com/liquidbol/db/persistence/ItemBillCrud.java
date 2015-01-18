1b57f6f7597bfd4840942d65cbe1bf53bcd88864
Allan Leon <allan.leon@outlook.com>
2015-01-17 21:50:12 -0400

    Created item bills crud

---
+++ src/com/liquidbol/db/persistence/ItemBillCrud.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.liquidbol.db.persistence;
+
+import com.liquidbol.model.commons.Employee;
+import com.liquidbol.model.commons.ItemBill;
+import com.liquidbol.model.commons.Store;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Class responsible of all persistence operations related to item bills.
+ * @author Allan Leon
+ */
+public class ItemBillCrud implements DBCrud<ItemBill> {
+    
+    private static final Logger LOG = Logger.getLogger(ItemBillCrud.class.getName());
+
+    private Connection connection;
+
+    @Override
+    public ItemBill save(ItemBill element) throws PersistenceException, ClassNotFoundException {
+        try {
+            connection = ConnectionManager.getInstance().getConnection();
+            String insert = "INSERT INTO item_bills(client_id, store_id, employee_id, "
+                    + "bill_date, total_ammount, is_route, obs) VALUES(?,?,?,?,?,?,?)";
+            PreparedStatement statement = connection.prepareCall(insert);
+            statement.setInt(1, 0);
+            statement.setInt(2, element.getStore().getId());
+            statement.setInt(3, element.getEmployee().getId());
+            statement.setDate(4, element.getDate());
+            statement.setDouble(5, element.getTotalAmmount());
+            statement.setBoolean(6, element.isRoute());
+            statement.setString(7, element.getObs());
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new PersistenceException("item bill was not saved");
+            }
+            LOG.info(String.format("item bill: %d successfuly saved", element.getId()));
+            return element;
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new PersistenceException(String.format("Failed to save item bill: %d", element.getId()), ex);
+        } finally {
+            try {
+                ConnectionManager.getInstance().releaseConnection();
+            } catch (SQLException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public ItemBill find(String id) throws PersistenceException, ClassNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ItemBill find(int id) throws PersistenceException, ClassNotFoundException {
+        try {
+            String query = "SELECT * FROM item_bills WHERE itembill_id = ?";
+            connection = ConnectionManager.getInstance().getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return createElementFromResultSet(resultSet);
+            } else {
+                throw new PersistenceException(String.format("Couldn't find item bill with code %d", id));
+            }
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Failed to read item bill", ex);
+        } finally {
+            try {
+                ConnectionManager.getInstance().releaseConnection();
+            } catch (SQLException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public ItemBill merge(ItemBill element) throws PersistenceException, ClassNotFoundException {
+        try {
+            String query = "UPDATE item_bills SET bill_date=?, total_ammount=?, "
+                    + "is_route=?, obs=? WHERE itembill_id=?";
+            PreparedStatement statement = 
+                ConnectionManager.getInstance().getConnection().prepareStatement(query);
+            statement.setDate(1, element.getDate());
+            statement.setDouble(2, element.getTotalAmmount());
+            statement.setBoolean(3, element.isRoute());
+            statement.setString(4, element.getObs());
+            statement.setInt(5, element.getId());
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new PersistenceException("item bill was not updated");
+            }
+            return element;
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new PersistenceException(String.format("Failed to update item bill: %d", element.getId()), ex);
+        } finally {
+            try {
+                ConnectionManager.getInstance().releaseConnection();
+            } catch (SQLException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public Collection<ItemBill> getAll() throws PersistenceException, ClassNotFoundException {
+        try {
+            String query = "SELECT * FROM item_bills";
+            connection = ConnectionManager.getInstance().getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            Collection<ItemBill> result = new HashSet<>();
+            while (resultSet.next()) {
+                ItemBill element = createElementFromResultSet(resultSet);
+                result.add(element);
+            }
+            return result;
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new PersistenceException("Failed to read the item bills", ex);
+        } finally {
+            try {
+                ConnectionManager.getInstance().releaseConnection();
+            } catch (SQLException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public ItemBill refresh(ItemBill element) throws PersistenceException, ClassNotFoundException {
+        return find(element.getId());
+    }
+
+    @Override
+    public ItemBill createElementFromResultSet(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt(1);
+        Store store = null;
+        Employee employee = null;
+        Date date = resultSet.getDate(5);
+        Double totalAmmount = resultSet.getDouble(6);
+        boolean route = resultSet.getBoolean(7);
+        String obs = resultSet.getString(8);
+        LOG.log(Level.FINE, "Creating item bill %d", id);
+        ItemBill result = new ItemBill(id, employee, date, obs, store, route);
+        return result;
+    }
+}
