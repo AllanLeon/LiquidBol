25ae7cade8d320388a2e5294e8c5b763c43b7053
Allan Leon <allan.leon@outlook.com>
2015-01-21 15:53:40 -0400

    Created company class

---
+++ src/com/liquidbol/model/commons/Company.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.liquidbol.model.commons;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * Class that represents LiquidBol company.
+ * @author Allan Leon
+ */
+public class Company {
+    
+    private Collection<Client> clients;
+    private Collection<Item> items;
+    private Collection<Service> services;
+    private Collection<Offer> offers;
+    private Collection<Store> stores;
+    private Collection<Supplier> suppliers;
+
+    /**
+     * Constructor method.
+     */
+    public Company() {
+        this.clients = new HashSet<>();
+        this.items = new HashSet<>();
+        this.services = new HashSet<>();
+        this.offers = new HashSet<>();
+        this.stores = new HashSet<>();
+        this.suppliers = new HashSet<>();
+    }
+
+    /**
+     * @return the clients
+     */
+    public Collection<Client> getAllClients() {
+        return clients;
+    }
+
+    /**
+     * @return the items
+     */
+    public Collection<Item> getAllItems() {
+        return items;
+    }
+
+    /**
+     * @return the services
+     */
+    public Collection<Service> getAllServices() {
+        return services;
+    }
+
+    /**
+     * @return the offers
+     */
+    public Collection<Offer> getAllOffers() {
+        return offers;
+    }
+
+    /**
+     * @return the stores
+     */
+    public Collection<Store> getAllStores() {
+        return stores;
+    }
+
+    /**
+     * @return the suppliers
+     */
+    public Collection<Supplier> getAllSuppliers() {
+        return suppliers;
+    }
+    
+    public void addClient(Client client) {
+        clients.add(client);
+    }
+    
+    public void addItem(Item item) {
+        items.add(item);
+    }
+    
+    public void addService(Service service) {
+        services.add(service);
+    }
+    
+    public void addOffer(Offer offer) {
+        offers.add(offer);
+    }
+    
+    public void addStore(Store store) {
+        stores.add(store);
+    }
+    
+    public void addSupplier(Supplier supplier) {
+        suppliers.add(supplier);
+    }
+}
