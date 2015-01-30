a2ba8e4f2c62c5d21a47aee8b59f32056a2da9ed
Allan Leon <allan.leon@outlook.com>
2015-01-30 00:18:30 -0400

    Created and implemented ItemTableModel

---
+++ src/com/liquidbol/gui/model/ItemTableModel.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.liquidbol.gui.model;
+
+import com.liquidbol.model.Item;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * Represents a table of items.
+ * @author Allan Leon
+ */
+public class ItemTableModel extends AbstractTableModel {
+    
+    private static final String[] COLUMN_NAMES = {"Nro.", "Cod.", "Unidad", "Descripcion",
+        "Marca", "Industria", "Tipo", "Subtipo", "Costo", "Precio"};
+    
+    private final List<Item> items;
+
+    public ItemTableModel(List<Item> item) {
+        this.items = item;
+    }
+    
+    @Override
+    public Class getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return Integer.class;
+            case 8: case 9:
+                return Double.class;
+            default :
+                return String.class;
+        }
+    }
+    
+    @Override
+    public Object getValueAt(int row, int column) {
+        
+        Item item = items.get(row);
+        switch (column) {
+            case 0:
+                return row + 1;
+            case 1:
+                return item.getId();
+            case 2:
+                return item.getMeasure();
+            case 3:
+                return item.getDescription();
+            case 4:
+                return item.getBrand();
+            case 5:
+                return item.getIndustry();
+            case 6:
+                return item.getType();
+            case 7:
+                return item.getSubtype();
+            case 8:
+                return item.getCost();
+            case 9:
+                return item.getPrice();
+            default:
+                return null;
+                
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return COLUMN_NAMES[column];
+    }
+
+    @Override
+    public int getColumnCount() {
+        return COLUMN_NAMES.length;
+    }
+
+    @Override
+    public int getRowCount() {
+        return items.size();
+    }
+}
